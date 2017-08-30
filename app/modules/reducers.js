388306c34d56aa6bd557c120d6ad67888d0609e4
Martin Kmieciak <mkmieciak@apptension.com>
2017-08-30 10:14:07 +0200

    Create register form component

--- app/modules/reducers.js
+++ app/modules/reducers.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux-immutable';
-import { reducer as formReducer } from 'redux-form';
 
 import { reducer as routerReducer } from './router/router.redux';
 
 export default function createReducer() {
   return combineReducers({
     route: routerReducer,
-    form: formReducer,
   });
 }
